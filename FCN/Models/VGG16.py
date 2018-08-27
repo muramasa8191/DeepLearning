9bf557ed256d39da969dac8d2ff44d6f060eae42
masa <muramasa8191@MacBook-Air.local>
2018-08-27 22:37:16 +0900

    [fix]for multi GPU

--- FCN/Models/VGG16.py
+++ FCN/Models/VGG16.py
@@ -1,7 +1,7 @@
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, Concatenate, Activation, Add, BatchNormalization
-from tensorflow.python.keras.applications.vgg16 import VGG16
-from tensorflow.python.keras.regularizers import l2
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, Concatenate, Activation, Add, BatchNormalization
+from keras.applications.vgg16 import VGG16
+from keras.regularizers import l2
 from layers.BilinearUpSampling2D import BilinearUpSampling2D
 import sys
 import os
@@ -50,23 +50,23 @@ def FCN_VGG16(input_shape, train=False, weight_decay=0., classes=22):
     x = Conv2D(4096, (1, 1), kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), activation='relu', padding='same', name="fc2")(x)
     x = Dropout(0.5, name="fc2_dropout")(x)
 
-    x = Conv2D(classes, (1, 1), kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), activation='relu', padding='same', name="fc_conv")(x)
+#    x = Conv2D(classes, (1, 1), kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), activation='relu', padding='same', name="fc_conv")(x)
 
     # p5
-    p5 = Conv2D(classes, (1, 1), kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), activation='relu', padding='same', name= "p5_conv")(x)
-    p5 = BilinearUpSampling2D(pool4.shape, data_format='channels_last', name="p5_upsampling")(p5)
+    p5 = Conv2D(classes, (1, 1), kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), activation='softmax', padding='same', name= "p5_conv")(x)
+    p5 = BilinearUpSampling2D((2, 2), data_format='channels_last', name="p5_upsampling")(p5)
     # p4
-    p4 = Conv2D(classes, (1, 1), activation='relu', padding='same', name="p4_conv")(pool4)
+    p4 = Conv2D(classes, (1, 1), activation='softmax', padding='same', name="p4_conv")(pool4)
     
     merge_p4p5 = Add(name="p4p5_add")([p5, p4])
-    merge_p4p5 = BilinearUpSampling2D(pool3.shape, data_format='channels_last', name="p4p5_upsampling")(merge_p4p5)
+    merge_p4p5 = BilinearUpSampling2D((2, 2), data_format='channels_last', name="p4p5_upsampling")(merge_p4p5)
     # p3
-    p3 = Conv2D(classes, (3, 3), kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), padding='same', name="p3_conv")(pool3)
+    p3 = Conv2D(classes, (3, 3), activation='softmax', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay), padding='same', name="p3_conv")(pool3)
     
     merge_p3p4p5 = Add(name="p3p4p5_add")([merge_p4p5, p3])
     
     # upsampling
-    output = BilinearUpSampling2D(input_image.shape, data_format='channels_last', name="output_upsampling")(merge_p3p4p5)
+    output = BilinearUpSampling2D((8, 8), data_format='channels_last', name="output_upsampling")(merge_p3p4p5)
 #        output = Conv2DTranspose(21, (3, 3), strides=(8, 8), activation='relu', padding='same', name="out_deconv")(merge_p3p4p5)
     output = Activation('softmax', name="out")(output)
 
--- FCN/layers/BilinearUpSampling2D.py
+++ FCN/layers/BilinearUpSampling2D.py
@@ -1,21 +1,43 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.keras import backend as K
-from tensorflow.python.keras.utils import conv_utils
-from tensorflow.python.keras.layers import Layer
-from tensorflow.python.keras.engine import InputSpec
+from keras import backend as K
+from keras.utils import conv_utils
+from keras.layers import Layer
+from keras.engine import InputSpec
 
 def bilinear_resize_images(x, size, method='bilinear'):
     new_size = tf.convert_to_tensor(size, dtype=tf.int32)
     resized = tf.image.resize_images(x, new_size)
     return resized
 
+def resize_images(x, height_factor, width_factor, data_format):
+    if data_format == 'channels_first':
+        original_shape = K.int_shape(x)
+        new_shape = tf.shape(x)[2:]
+        new_shape *= tf.constant(np.array([height_factor, width_factor]).astype('int32'))
+        x = permute_dimensions(x, [0, 2, 3, 1])
+        x = tf.image.resize_bilinear(x, new_shape)
+        x = permute_dimensions(x, [0, 3, 1, 2])
+        x.set_shape((None, None, original_shape[2] * height_factor if original_shape[2] is not None else None,
+                     original_shape[3] * width_factor if original_shape[3] is not None else None))
+        return x
+    elif data_format == 'channels_last':
+        original_shape = K.int_shape(x)
+        new_shape = tf.shape(x)[1:3]
+        new_shape *= tf.constant(np.array([height_factor, width_factor]).astype('int32'))
+        x = tf.image.resize_bilinear(x, new_shape)
+        x.set_shape((None, original_shape[1] * height_factor if original_shape[1] is not None else None,
+                     original_shape[2] * width_factor if original_shape[2] is not None else None, None))
+        return x
+    else:
+        raise ValueError('Unknown data_format: ' + str(data_format))
+
 class BilinearUpSampling2D(Layer):
     """ UpSampling2D uses nearest neighbor.
         This provides resize with bilinear interpolation.
     """
 
-    def __init__(self, target_shape=None, data_format=None, **kwargs):
+    def __init__(self, size=(2, 2), data_format=None, **kwargs):
         """ Constructor
         Parameters
         ----------
@@ -23,28 +45,30 @@ class BilinearUpSampling2D(Layer):
             Output shape instead of factors
         data_format: channels_last or channels_first
         """
-        if data_format is None:
-            data_format = K.image_data_format()
-        assert data_format in ['channels_last', 'channels_first']
-        self.data_format = data_format
-        self.input_spec = InputSpec(ndim=4)
-        self.target_shape = target_shape
-        if self.data_format == 'channels_first':
-            self.target_size = (target_shape[2], target_shape[3])
-        elif self.data_format == 'channels_last':
-            self.target_size = (target_shape[1], target_shape[2])
         super(BilinearUpSampling2D, self).__init__(**kwargs)
+        
+        self.data_format = K.normalize_data_format(data_format)
+        self.input_spec = InputSpec(ndim=4)
+        self.size = conv_utils.normalize_tuple(size, 2, 'size')
 
     def compute_output_shape(self, input_shape):
-        if self.data_format == 'channels_last':
-            return (input_shape[0], self.target_size[0],
-                    self.target_size[1], input_shape[3])
-        else:
-            return (input_shape[0], input_shape[1],
-                    self.target_size[0], self.target_size[1])
+        if self.data_format == 'channels_first':
+            height = self.size[0] * input_shape[2] if input_shape[2] is not None else None
+            width = self.size[1] * input_shape[3] if input_shape[3] is not None else None
+            return (input_shape[0],
+                    input_shape[1],
+                    height,
+                    width)
+        elif self.data_format == 'channels_last':
+            height = self.size[0] * input_shape[1] if input_shape[1] is not None else None
+            width = self.size[1] * input_shape[2] if input_shape[2] is not None else None
+            return (input_shape[0],
+                    height,
+                    width,
+                    input_shape[3])
 
     def call(self, inputs):
-        return bilinear_resize_images(inputs, size=self.target_size, method='bilinear')
+        return resize_images(inputs, self.size[0], self.size[1], self.data_format)
 
     def get_config(self):
         config = {'target_shape': self.target_shape, 'data_format': self.data_format}
